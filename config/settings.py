596ee842ae736b1bbb5dc1af2452059d16a6a1f0
yanfu837 <k256ssn@gmail.com>
2026-09-16 12:52:55 +0800

    Fix login redirect settings

--- config/settings.py
+++ config/settings.py
@@ -152,8 +152,11 @@ MAILERS = {
         },
     },
 }
+LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "login"
+
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"

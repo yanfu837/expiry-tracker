d20c24f58f09c710bf172d77e7ebae32dcfdc7be
yanfu837 <k256ssn@gmail.com>
2026-09-16 15:06:58 +0800

    Fix Cloudinary storage integration

--- config/settings.py
+++ config/settings.py
@@ -44,11 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-
-    'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary',
-
+   
     'expiry',
 ]
 

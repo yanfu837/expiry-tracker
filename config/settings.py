38969ff1d6f02022950b580a098a4e9605525fdb
yanfu837 <k256ssn@gmail.com>
2026-09-16 14:27:34 +0800

    Move secret key to environment variable

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n!0j=n5ow65si&7kosnw7n@vgw2fea!tiqfi&v3_mn)+rt5ff#'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
